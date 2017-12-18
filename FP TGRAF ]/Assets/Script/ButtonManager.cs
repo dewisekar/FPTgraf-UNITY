5b3022c8929e3c3c3df46c6ddb04d4fcea9e81f4
dewisekar <sekar.6499@gmail.com>
2017-12-18 14:34:49 +0700

    Coba Menu

---
+++ FP TGRAF ]/Assets/Script/ButtonManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+	// Use this for initialization
+	public void ToCredit()
+	{
+		SceneManager.LoadScene("Credit");	
+	}
+	public void ToMenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+	public void ToOption()
+	{
+		SceneManager.LoadScene("Options");
+	}
+	public void ExitButton()
+	{
+		Application.Quit();
+	}
+}
